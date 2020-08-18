f2be82088dfbb14a385a4ddbf59d13e9d96abbfb
tatsuya <tatsuya.takemura@gmail.com>
2020-08-18 16:02:06 +0900

    修正

--- script.js
+++ script.js
@@ -28,6 +28,6 @@ video.addEventListener('play', () => {
     // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
 
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    console.debug("detections")
+    console.debug("あああああ")
   }, 100)
 })

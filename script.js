3740f1f1d6cd44c6030f1d0514325a61f890ca69
tatsuya <tatsuya.takemura@gmail.com>
2020-08-18 12:49:38 +0900

    修正

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.load('/models'),
-  faceapi.nets.faceLandmark68Net.load('/models'),
-  faceapi.nets.faceRecognitionNet.load('/models'),
-  faceapi.nets.faceExpressionNet.load('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {

e72398bd83653595bae84816dae1b3f2218e8c64
tatsuya <tatsuya.takemura@gmail.com>
2020-08-18 16:58:36 +0900

    修正

--- script.js
+++ script.js
@@ -30,12 +30,26 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
     if (resizedDetections[0] != null) {
-       console.debug("resizeDetections",resizedDetections[0].expressions)
-       //let disguested = resizedDetections[0].expressions.disguested
+       console.debug("resizeDetections",resizedDetections[0].expressions.disgusted)
+       //let disgusted = resizedDetections[0].expressions.disgusted
 
        //FaceExpressions {neutral: 0.8571630120277405, happy: 0.0009491919190622866, sad: 0.0713937059044838, angry: 0.06651019304990768, fearful: 0.0018031822983175516, …}
-
+      //  FaceExpressions {neutral: 0.9998452663421631, happy: 9.013033377414104e-7, sad: 0.0000016673717482262873, angry: 0.00003788969843299128, fearful: 1.1844626390811186e-9, …}
+      //  angry: 0.00003788969843299128
+      //  disgusted:
+      //  6.673845476257156
+      //  e-9
+      //  fearful:
+      //  1.1844626390811186
+      //  e-9
+      //  happy:
+      //  9.013033377414104
+      //  e-7
+      //  neutral: 0.9998452663421631
+      //  sad: 0.0000016673717482262873
+      //  surprised: 0.00011418635403970256
       }
+
      
 
 
